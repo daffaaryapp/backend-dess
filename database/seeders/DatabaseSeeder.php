ff8f828fcbc218aa278a42af457b753f4b3deb87
teknologi <daffaaryapp@gmail.com>
2024-03-06 14:38:26 +0700

    membuat seeder permissions

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(RolesTableSeeder::class);
+        $this->call(PermissionsTableSeeder::class);
     }
 }
